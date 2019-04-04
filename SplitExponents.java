94dc32c9743de84ae87e7a5f921e222e11bf7687
Prabhjit Singh <pj92singh@gmail.com>
2019-04-04 17:34:54 -0400

    Update SplitExponents.java

--- SplitExponents.java
+++ SplitExponents.java
@@ -5,6 +5,8 @@
 *
 * Cracking code example String exponents 
 */
+import java.util.*;
+import java.util.Arrays;
 
 public class SplitExponents{
 	
@@ -12,21 +14,28 @@ public class SplitExponents{
 	public String in = " ";
 	
 	//2^3 3^4 4^5 5^6
+	//test mystring func
+	public static void main(String[] args){
+		mystring("2^3 3^4 4^5"); 
+	
+	}
 
 
-public void mystring(String s){
+public static void mystring(String s){
 	//take in the string
 	//break it down 
-	String[] splitString = s.split("\\s+"); 
-	System.out.println(Ararys.toString(splitString)); 
+	int[] xa = null; 
+	String[] splitString = s.split("\\s"); 
+	System.out.println("Split array string: " +Arrays.toString(splitString)); 
+	String[] splitString1 = s.split("^"); 
+	System.out.println("Split array string: " +Arrays.toString(splitString1)); 
 
 	try{
 
-		for (int x = 0; x > splitString.length-1; --x){
-			
-
-
+		for (int x = 0; x < splitString.length-1; x++){
+			xa[x] = Integer.parseInt(splitString[x]);
 		}
+		System.out.println("XA: " +Arrays.toString(xa)); 		
 	}catch(Exception e){
 		System.out.println("Error: " +e); 
 
