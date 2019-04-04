ac96d7deb2c77e8363de9724bea71f6440473a58
Prabhjit Singh <pj92singh@gmail.com>
2019-04-04 07:51:12 -0400

    Add files via upload
    testing splitting strings/exponents

---
+++ SplitExponents.java
@@ -0,0 +1,34 @@
+/* 
+* pj92singh
+* Prabhjit Singh 
+*
+*
+* Cracking code example String exponents 
+*/
+
+public class SplitExponents{
+	
+	public double n1, exp;
+	public String in = " ";
+	
+	//2^3 3^4 4^5 5^6
+
+}
+public mystring(String s){
+	//take in the string
+	//break it down 
+	String[] splitString = s.split("\\s+"); 
+	System.out.println(Ararys.toString(splitString)); 
+
+	try{
+
+		for (int x = 0; x > splitString.lenth()-1; --x){
+			
+
+
+		}
+	}catch(Exception e){
+		System.out.println("Error: " +e); 
+
+	}
+}
